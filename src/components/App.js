9221024052eb701b460db8821ef01f2abb002312
ajith <ajithraveendranr@gmail.com>
2022-07-26 17:47:12 +0530

    list in app

--- src/components/App.js
+++ src/components/App.js
@@ -38,12 +38,22 @@ class App extends Component {
     ]
     
   }
+  
   render() {
+    let list = document.getElementsByTagName('ol');
+    console.log( list );
+let k =0;
     return (
       <div id="main">
         <ol>
+        
         {
-         <List List={this.cityList}/>
+
+this.cityList.map(e => {
+  let val = `location${++k}`;
+    return e.country == 'India' && <li key={val}>{e.name}</li> 
+})
+         
             }
         </ol>
       </div>
