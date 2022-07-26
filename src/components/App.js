10003aba31a572a6beea2f43b43bb479713feaf4
AjithAj10 <104498680+AjithAj10@users.noreply.github.com>
2022-07-26 09:25:00 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -45,8 +45,7 @@ class App extends Component {
 // let k =0;
     return (
       <div id="main">
-        <ol><li key='location0'>goa</li>
-          {<List List={this.cityList}/>}</ol>
+        <ol>{<List List={this.cityList}/>}</ol>
       </div>
     )
   }
